38d0434c4ff3211916d46faf417311a024f1c8b1
William <mack.mindenhall@gmail.com>
2020-07-16 00:07:29 +0000

    Added login page, login route, basic users object and login buttons

--- server/index.js
+++ server/index.js
@@ -2,13 +2,15 @@
 
 // Basic express setup:
 
-const PORT          = 8080;
-const express       = require("express");
-const bodyParser    = require("body-parser");
-const app           = express();
+const PORT = 8080;
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
+app.set("view engine", "ejs");
 
 // The in-memory database of tweets. It's a basic object with an array in it.
 const db = require("./lib/in-memory-db");
@@ -26,8 +28,35 @@ const DataHelpers = require("./lib/data-helpers.js")(db);
 // so it can define routes that use it to interact with the data layer.
 const tweetsRoutes = require("./routes/tweets")(DataHelpers);
 
+const users = {
+  "NicolasCageSupreme": {
+    id: "NicolasCageSupreme",
+    email: "thenicolascage@thecage.com",
+    password: "the-cage",
+  },
+  "JonSnow": {
+    id: "JonSnow",
+    email: "snowyguy@thesnow.com",
+    password: "the-snow",
+  },
+};
+
 // Mount the tweets routes at the "/tweets" path prefix:
 app.use("/tweets", tweetsRoutes);
+
+app.get("/login", (req, res) => {
+  // console.log("response", req);
+  res.render("../views/login");
+});
+
+app.post("/login", (req, res) => {
+  const { username, password } = req.body;
+  console.log("req body", req.body);
+  if (users[username] && users[username].password === password) {
+    console.log("Success!");
+    res.redirect("/");
+  }
+});
 
 app.listen(PORT, () => {
   console.log("Example app listening on port " + PORT);
